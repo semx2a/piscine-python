940d45f6e3708cd6e0c2cdcc46057720e8681505
Semiha Beyazkilic <seozcan@student.42.fr>
2024-02-23 17:07:24 +0100

    fix: error handling

--- py00/ex00/Hello.py
+++ py00/ex00/Hello.py
@@ -3,25 +3,42 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
+""" ft_list = []
+ft_tuple = ()
+ft_set = {}
+ft_dict = {}
+ """
 # modify list
 world = "World!"
-ft_list[-1] = world
+try:
+	ft_list[-1] = world
+except:
+	print("Error: Issue with list!")
 
 # modify tuple
-new_tuple = list(ft_tuple)
-new_tuple.remove("toto!")
-ft_tuple = tuple(new_tuple)
-country = ("France!",)
-ft_tuple += country
+try:
+	new_tuple = list(ft_tuple)
+	new_tuple.remove("toto!")
+	ft_tuple = tuple(new_tuple)
+	country = ("France!",)
+	ft_tuple += country
+except:
+	print("Error: Issue with tupple")
 
 # modify set
-city = "Paris!"
-ft_set.remove("tutu!")
-ft_set.add(city)
+try:
+	city = "Paris!"
+	ft_set.remove("tutu!")
+	ft_set.add(city)
+except:
+	print("Error: Issue with set!")
 
 # modify dict
-campus = "42Paris!"
-ft_dict["Hello"] = campus
+try:
+	campus = "42Paris!"
+	ft_dict["Hello"] = campus
+except:
+	print("Error: Issue with dict!")
 
 print(ft_list)
 print(ft_tuple)
