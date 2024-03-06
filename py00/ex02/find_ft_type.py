dfa65863c651255bab483e32770992d02b76c013
Semiha Beyazkilic <seozcan@student.42.fr>
2024-03-06 18:42:14 +0100

    fix: error handling

--- py00/ex02/find_ft_type.py
+++ py00/ex02/find_ft_type.py
@@ -2,21 +2,23 @@ def all_thing_is_obj(object: any) -> int:
 	obj_type = type(object)
 	ret = None
 
-	match object:
-		case str():
-			ret = f'{object} is in the kitchen : {obj_type}'
-		case list():
-			ret = f'List: {obj_type}'
-		case tuple():
-			ret = f'Tuple: {obj_type}'
-		case set():
-			ret = f'Set: {obj_type}'
-		case dict():
-			ret = f'Dict: {obj_type}'
-		case _:
-			ret = "Type not found"
-	
-	if ret is not None:
-		print(ret)
+	try:
+		match object:
+			case str():
+				ret = f'{object} is in the kitchen : {obj_type}'
+			case list():
+				ret = f'List: {obj_type}'
+			case tuple():
+				ret = f'Tuple: {obj_type}'
+			case set():
+				ret = f'Set: {obj_type}'
+			case dict():
+				ret = f'Dict: {obj_type}'
+			case _:
+				ret = "Type not found"
 
+		if ret is not None:
+			print(ret)
+	except:
+		print("Error: undefined")
 	return 42
