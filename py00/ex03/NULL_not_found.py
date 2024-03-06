f921a09f892efa3328da9679edb1641575d43535
Semiha Beyazkilic <seozcan@student.42.fr>
2024-03-06 18:42:26 +0100

    fix: error handling

--- py00/ex03/NULL_not_found.py
+++ py00/ex03/NULL_not_found.py
@@ -6,26 +6,28 @@ def NULL_not_found(object: any) -> int:
 	output = None
 	ret = 1
 
-	if not object or obj_type == float and math.isnan(object):
-		match object:
-			case str():
-				output = 'Empty: '
-			case float():
-				output = 'Cheese: '
-			case bool():
-				output = 'Fake: '
-			case int():
-				output = 'Zero: '
-			case None:
-				output = 'Nothing: '
-			case _:
-				pass
-		output += f'{object} {obj_type}'
-		ret = 0
-	else:
-		output = "Type not found"
+	try:
+		if not object or (obj_type == float and math.isnan(object)):
+			match object:
+				case str():
+					output = 'Empty: '
+				case float():
+					output = 'Cheese: '
+				case bool():
+					output = 'Fake: '
+				case int():
+					output = 'Zero: '
+				case None:
+					output = 'Nothing: '
+				case _:
+					pass
+			output += f'{object} {obj_type}'
+			ret = 0
+		else:
+			output = "Type not found"
 
-	if output is not None:
-		print(output)
-	
+		if output is not None:
+			print(output)
+	except:
+		print("Error: undefined")
 	return ret
