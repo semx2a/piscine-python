d74439ce8e6c2de1cea48c5527c98b8286097bc1
Semiha Beyazkilic <seozcan@student.42.fr>
2024-03-11 18:23:22 +0100

    style: lint with flake8

--- py00/ex04/whatis.py
+++ py00/ex04/whatis.py
@@ -1,17 +1,23 @@
 import sys
 
-whatis = lambda number: print("I'm Even.") if number % 2 == 0 else print("I'm Odd.")
-	
-def main(argv):
-	try:
-		assert len(argv) <= 2, "more than one argument provided"
-		if len(argv) > 1:
-			assert argv[1][0] == "-" and argv[1][1:].isnumeric() or argv[1].isnumeric(), "argument is not an integer"
-			whatis(int(argv[1]))
 
-	except AssertionError as msg:
-		print(f'AssertionError: {msg}')
-		exit(1)
+def whatis():
+    lambda number: print("I'm Even.") if number % 2 == 0 else print("I'm Odd.")
+
+
+def main(argv):
+    try:
+        assert len(argv) <= 2, "more than one argument provided"
+        if len(argv) > 1:
+            assert argv[1][0] == "-" and argv[1][1:].isnumeric() \
+                or argv[1].isnumeric(), \
+                "argument is not an integer"
+            whatis(int(argv[1]))
+
+    except AssertionError as msg:
+        print(f'AssertionError: {msg}')
+        exit(1)
+
 
 if __name__ == "__main__":
-	main(sys.argv)
+    main(sys.argv)
