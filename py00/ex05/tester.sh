a0957d8f0c283bdc27033ef4368e58c5c5698711
Semiha Beyazkilic <seozcan@student.42.fr>
2024-03-11 18:28:31 +0100

    feat: tester

---
+++ py00/ex05/tester.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SEP="____________________________________"
+RESET=$(tput sgr0)
+BOLD=$(tput bold)
+
+function test
+{
+	echo $BOLD
+	echo "testing \"$1\""
+	echo -n -e "Result: $RESET"
+	python building.py $1
+	if [ -z "$1" ] 
+	then
+		echo -e 
+	fi
+	echo -e $SEP
+}
+
+test "Python 3.0, released in 2008, was a major revision that is not completely backwardcompatible with earlier versions. Python 2 was discontinued with version 2.7.18 in 2020."
+test "Hello World!"
+test ""
+test "123456789"
+test "!@#\$%^\&\*'('')'-=,./;'['']' "
