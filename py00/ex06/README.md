090a621729f1b2567ad0717399b764df7f14ca95
Semiha Beyazkilic <seozcan@student.42.fr>
2024-04-26 18:59:16 +0200

    feat: finished

---
+++ py00/ex06/README.md
@@ -0,0 +1,52 @@
+# Exercise 6: Remake Filter
+
+## Instructions
+
+Allowed functions: sys or any other library that allows you to receive args
+
+**Part 1:**
+Recode your own ft_filter, it should behave like the original built-in function (it should return the same thing as "print(filter.__doc__)"), you should use list comprehensions to recode your ft_filter.
+
+>Of course using the original filter built-in is forbidden
+
+**Part 2: The program**
+Create a program that accepts two arguments: a string(S), and an integer(N). The program should output a list of words from S that have a length greater than N.
+
+- Words are separated from each other by space characters.
+- Strings do not contain any special characters. (Punctuation or invisible)
+- The program must contain at least one list comprehension expression and one lambda.
+- If the number of argument is different from 2, or if the type of any argument is wrong, the program prints an AssertionError
+
+**Expected outputs:**
+
+```bash
+$> python filterstring.py 'Hello the World' 4
+['Hello', 'World']
+$>
+```
+
+```bash
+$> python filterstring.py 'Hello the World' 99
+[]
+$>
+```
+
+```bash
+$> python filterstring.py 3 'Hello the World'
+AssertionError: the arguments are bad
+$>
+```
+
+```bash
+$> python filterstring.py
+AssertionError: the arguments are bad
+$>
+```
+
+## References
+- [Python List Comprehension](https://www.w3schools.com/python/python_lists_comprehension.asp)
+- [Python Filter() function](https://www.w3schools.com/python/ref_func_filter.asp)
+- [Python Lambda](https://www.w3schools.com/python/python_lambda.asp)
+- [How to use Python Lambda Functions](https://realpython.com/python-lambda)
+- [Rationale of Python yield](https://www.reddit.com/r/learnpython/comments/rzukrb/what_is_the_idea_of_using_yield_in_python/)
+- [La fonction yield](https://developpement-informatique.com/article/149/la-fonction-yield)
---
+++ py00/ex06/filterstring.py
@@ -0,0 +1,74 @@
+import sys
+import string
+from ft_filter import ft_filter
+
+
+def is_needle(haystack: str):
+    """ is_needle() returns haystack needle type lambda """
+    return lambda needle: needle in haystack
+
+
+def not_printable():
+    """ not_printable() returns a lambda function to test if the character
+    is not printable """
+    return lambda char: char not in string.printable
+
+
+def is_greater(size: int):
+    """ is_greater() returns a lambda function to test if the word's length is
+    greater than the size """
+    return lambda word: len(word) > size
+
+
+def parse_string(S: str) -> None:
+    """ The parse_string() function tests the string for punctuation and
+    non printable characters. If either are found, the function raises an
+    AssertionError """
+    is_punctuation = is_needle(string.punctuation)
+    isnot_printable = not_printable()
+
+    punctuation_list = ft_filter(is_punctuation, S)
+    invisible_list = ft_filter(isnot_printable, S)
+
+    assert len(punctuation_list) == 0 \
+        and len(invisible_list) == 0, "the arguments are bad"
+
+
+def filterstring(S: str, N: int) -> list:
+    """
+    The filterstring() function returns a list of words from S that have a
+    length greater than N. The functions accepts two arguments:
+    - a string 'S''
+    - an integer 'N'
+    A word is separated by whitespaces.
+    """
+    parse_string(S)
+
+    words_list = S.split()
+    condition = is_greater(N)
+
+    filtered_list = ft_filter(condition, words_list)
+
+    return filtered_list
+
+
+def main(argv):
+
+    try:
+        assert len(argv) == 3 \
+            and argv[1].isascii() \
+            and argv[2].isnumeric(), "the arguments are bad"
+
+        filtered_list = filterstring(argv[1], int(argv[2]))
+        print(filtered_list)
+
+        # __doc__ tester
+        # print(f'ft_filter doc : {ft_filter.__doc__}')
+        # print(f'filter doc: {filter.__doc__}')
+
+    except AssertionError as msg:
+        print(f"AssertionError: {msg}")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
