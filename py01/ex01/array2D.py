c64d1ae7d825c7120ecaceac926ae080a88b71e6
Semiha Beyazkilic <seozcan@student.42.fr>
2024-06-25 15:29:13 +0200

    feat: safety features

--- py01/ex01/array2D.py
+++ py01/ex01/array2D.py
@@ -4,13 +4,15 @@ import numpy as np
 def slice_me(family: list, start: int, end: int) -> list:
 
     try:
-        family_array = np.array(family)
-        sliced_family = family_array[start:end]
+        if family:
+            family_array = np.array(family)
+            sliced_family = family_array[start:end]
 
-        print(f'My shape is : {family_array.shape}')
-        print(f'My new shape is: {sliced_family.shape}')
+            print(f'My shape is : {family_array.shape}')
+            print(f'My new shape is: {sliced_family.shape}')
+
+            return sliced_family.tolist()
 
     except Exception as msg:
         print(msg)
-
-    return sliced_family.tolist()
+        return []
