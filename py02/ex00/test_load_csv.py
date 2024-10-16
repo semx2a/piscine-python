d447eefed904f2f41c5903e506d7378375e21782
Semiha Beyazkilic <seozcan@student.42.fr>
2024-10-16 10:11:30 +0200

    style(tests): fix indentation in test_load_csv.py for flake8 consistency

--- py02/ex00/test_load_csv.py
+++ py02/ex00/test_load_csv.py
@@ -1,12 +1,13 @@
 import unittest
 from load_csv import load
 
+
 class TestLoad(unittest.TestCase):
-	def test_load_wrong_path(self):
-		pass
+    def test_load_wrong_path(self):
+        pass
 
-	def test_load_no_path(self):
-		pass
+    def test_load_no_path(self):
+        pass
 
-	def test_load_right_path(self):
-		load("../data/life_expctancy_years.csv")
+    def test_load_right_path(self):
+        load("../data/life_expctancy_years.csv")
