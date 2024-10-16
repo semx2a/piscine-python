8ebb1889712f6390b9ad6f48f47e8b5bd493040b
Semiha Beyazkilic <seozcan@student.42.fr>
2024-10-16 14:10:08 +0200

    refactor(tests): migrate from unittest to pytest and enhance test coverage

--- py02/ex00/test_load_csv.py
+++ py02/ex00/test_load_csv.py
@@ -1,13 +1,28 @@
-import unittest
+import pytest
+import os
 from load_csv import load
 
 
-class TestLoad(unittest.TestCase):
-    def test_load_wrong_path(self):
-        pass
+# Determine the absolute path to the assets directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+assets_dir = os.path.join(current_dir, '../data/')
 
-    def test_load_no_path(self):
-        pass
 
-    def test_load_right_path(self):
-        load("../data/life_expctancy_years.csv")
+@pytest.mark.parametrize('path', [
+    os.path.join(assets_dir,
+                 'income_per_person_gdppercapita_ppp_inflation_adjusted.csv'),
+    os.path.join(assets_dir, 'life_expectancy_years.csv'),
+    os.path.join(assets_dir, 'population_total.csv')
+])
+def test_load_runs_without_erros(path):
+    load(path)
+
+
+@pytest.mark.parametrize('path', [
+    os.path.join(assets_dir,
+                 'income_per_person_gdppercapita_ppp_inflation_adjusted.csv'),
+    os.path.join(assets_dir, 'life_expectancy_years.csv'),
+    os.path.join(assets_dir, 'population_total.csv')
+])
+def test_load_runs_with_errors(path):
+    load(path)
--- py02/ex00/tester.py
+++
@@ -1,9 +0,0 @@
-from load_csv import load
-
-print(load('../data/life_expectancy_years.csv'))
-print(load('../data/wrong_data/empty.csv'))
-print(load('../data/wrong_data/invalid_data.csv'))
-print(load('../data/wrong_data/char.csv'))
-print(load('../data/wrong_data/\
-           income_per_gdpcapita_ppp_inflation_adjusted.xlsx'))
-print(load('../../py01/assets/animal.jpeg'))
