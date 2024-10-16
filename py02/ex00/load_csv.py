77cec54b5b0b6b572edd0bdd14fbc98eb1dc1532
Semiha Beyazkilic <seozcan@student.42.fr>
2024-10-16 16:59:49 +0200

    fix(load_csv): improve error handling and add docstring for load function

--- py02/ex00/load_csv.py
+++ py02/ex00/load_csv.py
@@ -2,21 +2,26 @@ import pandas as pd
 
 
 def load(path: str) -> pd.DataFrame:
+    """
+    load takes a csv file path and returns a data frame.
 
+    Parameters:
+    path (str): path of the csv file.
+
+    Return Value:
+    dataframe (Pandas DataFrame): a dataframe containing the csv's content.
+    """
     try:
+        # Load csv data into a dataframe
         dataframe = pd.read_csv(path)
+        if dataframe.isnull().values.any():
+            raise ValueError("CSV file contains malformed rows")
         print(f'Loading dataset of dimensions {dataframe.shape}')
         return dataframe
 
-    except pd.errors.EmptyDataError as ede:
-        print(f'Empty data: {ede}')
-    except pd.errors.ParserWarning as pw:
-        print(f'Parser Warning: {pw}')
-    except pd.errors.IncompatibilityWarning as iw:
-        print(f'Incompatibility Warning: {iw}')
-    except UnicodeDecodeError as ude:
-        print(f'Decoding error: {ude}')
+    except pd.errors.EmptyDataError:
+        exit(f"Error: The file {path} is empty.")
+    except pd.errors.ParserError:
+        exit(f"Error: The file {path} contains invalid data.")
     except Exception as e:
-        print(f'Exception: {e}')
-
-    return None
+        exit(f'Exception: {e}')
--- py02/ex00/test_load_csv.py
+++ py02/ex00/test_load_csv.py
@@ -1,11 +1,13 @@
 import pytest
 import os
+import pandas as pd
 from load_csv import load
 
 
 # Determine the absolute path to the assets directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 assets_dir = os.path.join(current_dir, '../data/')
+wrong_data = os.path.join(assets_dir, 'wrong_data/')
 
 
 @pytest.mark.parametrize('path', [
@@ -14,15 +16,24 @@ assets_dir = os.path.join(current_dir, '../data/')
     os.path.join(assets_dir, 'life_expectancy_years.csv'),
     os.path.join(assets_dir, 'population_total.csv')
 ])
-def test_load_runs_without_erros(path):
-    load(path)
+def test_load_runs_without_errors(path):
+    df = load(path)
+    assert isinstance(df, pd.DataFrame)
+    assert not df.empty
 
 
 @pytest.mark.parametrize('path', [
-    os.path.join(assets_dir,
-                 'income_per_person_gdppercapita_ppp_inflation_adjusted.csv'),
-    os.path.join(assets_dir, 'life_expectancy_years.csv'),
-    os.path.join(assets_dir, 'population_total.csv')
+    os.path.join(wrong_data,
+                 'income_per_gdpcapita_ppp_inflation_adjusted.xlsx'),
+    os.path.join(wrong_data, 'invalid_data.csv'),
+    os.path.join(wrong_data, 'char.csv'),
+    os.path.join(wrong_data, 'empty.csv'),
+    'README.md',
+    '',
+    ' '
 ])
 def test_load_runs_with_errors(path):
-    load(path)
+    with pytest.raises((SystemExit,
+                        pd.errors.EmptyDataError,
+                        pd.errors.ParserError)):
+        load(path)
