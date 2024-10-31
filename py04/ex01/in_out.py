05f6d34e853f8169cc62277d21943a9dea56659d
Semiha Beyazkilic <seozcan@student.42.fr>
2024-10-31 15:35:25 +0100

    feat(ex01, ex02): enrich README and add docstrings for clarity in functions

--- py04/ex01/in_out.py
+++ py04/ex01/in_out.py
@@ -13,9 +13,18 @@ def pow(x: int | float) -> int | float:
 
 
 def outer(x: int | float, function) -> object:
+    """outer is an enclosure that returns a nested function
+
+    Parameters:
+    x = integer or float to be computed by the inner function
+    function = function to be used to perform operation
+    """
     count = 0
 
     def inner() -> float:
+        """nested function that counts the number of iterations and returns
+        the result of the operation from pow or square. It takes no
+        parameters"""
         try:
             nonlocal count
             base = function(x)
--- py04/ex02/callLimit.py
+++ py04/ex02/callLimit.py
@@ -1,9 +1,40 @@
 def callLimit(limit: int):
+    """
+    A decorator to limit the number of times a function can be called.
+
+    Parameters:
+    limit (int): The maximum number of times the decorated function can be
+    called.
+
+    Returns:
+    function: A decorator that limits the number of calls to the decorated
+    function.
+    """
     count = 0
 
     def callLimiter(function):
+        """
+        Inner decorator function that wraps the target function.
 
+        Parameters:
+        function (function): The function to be decorated.
+
+        Returns:
+        function: The wrapped function with call limiting applied.
+        """
         def limit_function(*args: any, **kwds: any):
+            """
+            Wrapper function that enforces the call limit.
+
+            Parameters:
+            *args (any): Positional arguments to pass to the target function.
+            **kwds (any): Keyword arguments to pass to the target function.
+
+            Returns:
+            function: The target function if the call limit has not been
+            exceeded.
+            Prints an error message if the call limit is exceeded.
+            """
             nonlocal count
             if count < limit:
                 count += 1
